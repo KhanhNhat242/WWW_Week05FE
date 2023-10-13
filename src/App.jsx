b70d5df96ed793821b1f3db33b3692c1d325e0fe
KhanhNhat242 <86726746+KhanhNhat242@users.noreply.github.com>
2023-10-13 21:33:40 +0700

    call API

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,20 @@ import Company from './component/company/Company'
 import Home from './component/home/Home'
 import Header from './component/header/Header'
 import Job from './component/job/Job'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const [jobs, setJobs] = useState([])
+
+  const getData = async () => {
+    const jobs = await axios.get('http://localhost:8080/getAllJob')
+    setJobs(jobs.data)
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
 
   return (
     <div className='App'>
@@ -13,7 +25,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/company' element={<Company />} />
-          <Route path='/job' element={<Job />} />
+          <Route path='/job' element={<Job jobs={jobs} />} />
         </Routes>
       </Router>
     </div>
--- src/component/job/Job.jsx
+++ src/component/job/Job.jsx
@@ -1,18 +1,40 @@
 import './Job.scss'
 
-function Job() {
-    return ( 
+function Job({ jobs }) {
+
+
+    return (
         <div className='job-container'>
-            <div className="job-wrapper">
-                <div className='job-left'>
-                    <h1 className="skill-level">ADVANCE</h1>
+            {
+                jobs.map((j) => {
+                    return (
+                        <>
+                            <div className="job-wrapper">
+                                <div className='job-left'>
+                                    <h1 className="skill-level">ADVANCE</h1>
+                                </div>
+                                <div className="job-right">
+                                    <h2 className="job-name">{j.jobName}</h2>
+                                    <h4 className="company-name">{j.company.compName}</h4>
+                                    <p className="job-address">{j.company.address.city}</p>
+                                </div>
+                            </div>
+                        </>
+                    )
+                })
+            }
+            {/* <div className='job-container'>
+                <div className="job-wrapper">
+                    <div className='job-left'>
+                        <h1 className="skill-level">ADVANCE</h1>
+                    </div>
+                    <div className="job-right">
+                        <h2 className="job-name">Front-end Web Developer</h2>
+                        <h4 className="company-name">Company Name</h4>
+                        <p className="job-address">12 Nguyen Van Bao</p>
+                    </div>
                 </div>
-                <div className="job-right">
-                    <h2 className="job-name">Front-end Web Developer</h2>
-                    <h4 className="company-name">Company Name</h4>
-                    <p className="address">12 Nguyen Van Bao</p>
-                </div>
-            </div>
+            </div> */}
         </div>
      );
 }
